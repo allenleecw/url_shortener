77790e673cbd65dc8789b86528f12cf0648c9721
Allen <mabelsia@Mabels-MacBook-Air.local>
2017-07-05 10:33:41 +0800

    added the shorten url method

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,28 @@
+class UrlsController < ApplicationController
+  def index
+    @urls = Url.all
+  end
+
+  def show
+    @url = Url.find(params[:id])
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.new(url_params)
+    @url.short_url = Url.shorten_url
+    if @url.save
+      redirect_to @url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def url_params
+    params.require(:url).permit(:long_url)
+  end
+end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,8 @@
 class Url < ApplicationRecord
+  validates :long_url, format: {with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix}
+
+  def self.shorten_url
+    short_url = SecureRandom.hex(4)
+  end
+
 end
